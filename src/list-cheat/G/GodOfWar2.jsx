cb6d3930928d945c75586a7a0a365e966e67d8da
Eszuri <ualdo296@gmail.com>
2023-08-29 21:35:01 +0700

    324R34R34

---
+++ src/list-cheat/G/GodOfWar2.jsx
@@ -0,0 +1,40 @@
+import '../../Global.css'
+import Navbar from '../../components/Navbar'
+import Menu from '../../components/menu'
+import { Ps2Pagetitle } from '../../components/PS2-PAGE-TITLE'
+import Ps2PageStepUsage from '../../components/PS2-PAGE-STEP-USAGE'
+import Ps2PageControllerCode, { TextCaraPakai, TextCarapakaikodedengancontoller, TextH1 } from '../../components/PS2-PAGE-CONTROLLER-CODE'
+import { BoilerplateRawPNACH, LinkSourceCode, SourceCode } from '../../components/PS2-PAGE-CODE-PNACH'
+import Ps2PageDownloadLink from '../../components/PS2_PAGE_DOWNLOAD_LINK'
+import Bukamenu from '../../components/bukamenu'
+
+
+export default function GodOfWar() {
+    return (
+        <>
+            <Navbar />
+            <Menu />
+            <Ps2Pagetitle
+                judul='God Of War ||'
+                descJudul='God Of War ||'
+                descSerial='SCUS-97481'
+                descCRC='2F123FD8'
+                descRegion='NTSC - U' />
+            <Ps2PageStepUsage namaFile='2F123FD8.pnach' />
+            <div className='mt-32'>
+                <TextCarapakaikodedengancontoller />
+                <Ps2PageControllerCode Titlekode='Saat ini belom ada ' />
+            </div>
+            <BoilerplateRawPNACH urlFile="/2F123FD8.pnach" />
+            <SourceCode />
+            <LinkSourceCode urlSumberKode="https://codetwink.com/view-cheats-4452-title-god+of+war+ii.html" namaSumberKode="1. CodeTwink" />
+            <LinkSourceCode urlSumberKode="https://gamehacking.org/game/103833" namaSumberKode="2. GameHacking" />
+            <Ps2PageDownloadLink
+                namaFile="2F123FD8.pnach"
+                dateFile="29 Agustus 2023"
+                sizeFile="2,86 KB"
+            />
+            <Bukamenu />
+        </>
+    )
+}
