b151628b49cb515bd4ffc995c7537675adf1b5aa
Eszuri <ualdo296@gmail.com>
2023-09-23 14:32:57 +0700

    rthr

--- src/components/Changelog.jsx
+++ src/components/Changelog.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { Children } from 'react'
 
 export default function Changelog() {
     return (
         <>
             <div className='w-full bg-black p-2 text-left '>
                 <h1 className='text-2xl'>Log Perubahan {"(Changelog)"}</h1>
-                <BOXCHANGELOG date="2020" />
+                <BOXCHANGELOG tanggal="2023">
+                    <WebUpdate perubahan="COMING SOOOOOOON" />
+                </BOXCHANGELOG>
             </div >
         </>
     )
 }
 
-export function BOXCHANGELOG(props) {
+export function BOXCHANGELOG({ tanggal, children }) {
     return (
         <>
             <span className='absolute w-5 inline-block h-5 rounded-full border-4 bg-black border-emerald-600'></span>
             <div className='bg-stone-800 border-l-2 ml-2'>
-                <h1 className='ml-3'>{props.date}</h1>
+                <h1 className='ml-3'>{tanggal}</h1>
                 <div className='ml-5 mt-3'>
                     <h1 className='text-xl text-white underline decoration-yellow-600 decoration-[3px] font-bold'>Perubahan Web</h1>
-                    <p>otw lg dibuat</p>
+                    {children}
                 </div>
             </div>
+        </>
+    )
+}
+
+export function WebUpdate(props) {
+    return (
+        <>
+            <h1>{props.perubahan}</h1>
         </>
     )
 }
