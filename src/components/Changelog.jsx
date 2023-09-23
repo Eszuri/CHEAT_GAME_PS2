bd189f8e7b81c66a9e78d6fc86ee6b5adf849ae5
Eszuri <ualdo296@gmail.com>
2023-09-23 14:11:27 +0700

    efqwfwefwef

--- src/components/Changelog.jsx
+++ src/components/Changelog.jsx
@@ -3,20 +3,25 @@ import React from 'react'
 export default function Changelog() {
     return (
         <>
-            <div className='w-full bg-black p-2'>
-                <h1 className='text-left text-2xl'>Log Perubahan {"(Changelog)"}</h1>
-                <div className='mt-3 bg-stone-800 h-80 rounded'>
-                    <Bola />
-                </div>
-            </div>
+            <div className='w-full bg-black p-2 text-left '>
+                <h1 className='text-2xl'>Log Perubahan {"(Changelog)"}</h1>
+                <BOXCHANGELOG date="2020" />
+            </div >
         </>
     )
 }
 
-export function Bola() {
+export function BOXCHANGELOG(props) {
     return (
         <>
-            <span className='w-5 h-5 rounded-full bg-blue-600'></span>
+            <span className='absolute w-5 inline-block h-5 rounded-full border-4 bg-black border-emerald-600'></span>
+            <div className='bg-stone-800 border-l-2 ml-2'>
+                <h1 className='ml-3'>{props.date}</h1>
+                <div className='ml-5 mt-3'>
+                    <h1 className='text-xl text-white underline decoration-yellow-600 decoration-[3px] font-bold'>Perubahan Web</h1>
+                    <p>otw lg dibuat</p>
+                </div>
+            </div>
         </>
     )
 }
